ef1da2b9ab1cd885dbcdf61a6d6aece70735a4b5
Hanan <108991267+ihanmx@users.noreply.github.com>
2025-11-25 19:05:03 +0300

    Update database.js

--- helper/database.js
+++ helper/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node-backend",
-  password: "Han@Bz#3571",
+  password: "",
 });
 
 module.exports = pool.promise();

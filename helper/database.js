5f711cd4471dc59661bc0f497d5f151ea1bd90fb
Hanan <hanan.bayazeed56@gmail.com>
2025-11-25 18:59:48 +0300

    fix DB code

--- helper/database.js
+++ helper/database.js
@@ -1,1 +1,10 @@
-const Sequelize = require("sequelize");
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-backend",
+  password: "Han@Bz#3571",
+});
+
+module.exports = pool.promise();
